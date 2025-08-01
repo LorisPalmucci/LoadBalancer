0403784a28b0586372dc6828208036a30809b388
UncleJason88 <loris.palmucci@studenti.unicam.it>
2025-08-01 23:41:58 +0200

    build listener component

--- src/listener.py
+++ src/listener.py
@@ -1,10 +1,21 @@
 import socket
+from threading import Thread, ThreadError
 
 HOST = ''
 PORT = 50007
 
+def new_thread(conn, addr):
+    print ("connesso da ", addr)
+    print (conn)   
 
 def start_server():
-
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST,PORT))
+        s.listen(5)
+        while True:
+            conn, addr = s.accept()
+            t = Thread(target=new_thread, args=(conn,addr))
+            t.start()
+        
 if __name__ == "__main__":
     start_server()
