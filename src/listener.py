a619c311711975825bea0dd667f6f990b87e140a
UncleJason88 <loris.palmucci@studenti.unicam.it>
2025-08-02 12:41:18 +0200

    Add code comments

--- src/listener.py
+++ src/listener.py
@@ -9,10 +9,10 @@ def new_thread(conn, addr):
     print (conn)   
 
 def start_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST,PORT))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    #crea la socket
+        s.bind((HOST,PORT))                                         #assegna la socket ad una tupla IP:PORT e resta in ascolto
         s.listen(5)
-        while True:
+        while True:                                                 #attende una connessione e genera un thread quando arriva
             conn, addr = s.accept()
             t = Thread(target=new_thread, args=(conn,addr))
             t.start()
